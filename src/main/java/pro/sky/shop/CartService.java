11e0a64436c04d18173c8ee9b2240df0ccb035ea
Den <tequila96@bk.ru>
2023-11-18 16:20:19 +0500

    Исправил

--- src/main/java/pro/sky/shop/CartService.java
+++ src/main/java/pro/sky/shop/CartService.java
@@ -1,11 +1,13 @@
 package pro.sky.shop;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@SessionScope
 public class CartService {
     private final List<Integer> cartItems;
 
